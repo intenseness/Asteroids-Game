580cb980ef8c4209685e370cbb472ee5753549c1
Connor O Meny <cm73189@srcs.loc>
2019-04-18 13:28:04 -0700

    New outline for developing game

---
+++ GamePage.js
@@ -0,0 +1,91 @@
+class AttackOfThePizzas {
+    constructor() {
+        this.player = new Player();
+        this.alien = new Alien();
+        this.pizza = new Pizza();
+        this.lifeboard = new Lifeboard();
+        this.scoreboard = new Scoreboard();
+    }
+
+    updateLifeboard() {
+
+    }
+
+    displayLifeboard() {
+
+    }
+
+    updateScoreboard() {
+
+    }
+
+    displayScoreboard() {
+
+    }
+
+    spawnAlien() {
+
+    }
+
+    spawnPizza() {
+
+    }
+}
+
+class Player {
+    constructor() {
+        this.speed = ;
+        this.direction = ;
+        this.turnAngle = ;
+        this.laserSpeed = ;
+    }
+
+    shootLasers() {
+
+    }
+}
+
+class Alien {
+    constructor() {
+        this.speed = ;
+        this.direction = ;
+        this.turnAngle = ;
+        this.laserSpeed = ;
+    }
+
+    shootRandLasers() {
+
+    }
+}
+
+class Lifeboard {
+    constuctor() {
+        this.lives = ;
+    }
+
+    updateLives() {
+
+    }
+}
+
+class Scorebaord {
+    constuctor() {
+        this.score = ;
+    }
+
+    updateScore() {
+
+    }
+}
+
+class Pizza {
+    constuctor() {
+        this.levelOfSize = ;
+        this.speed = ;
+        this.direction = ;
+    }
+
+    breakOff() {
+
+    }
+}
