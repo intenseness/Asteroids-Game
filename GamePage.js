fa2833f43c2b76af165667d5db14489702f9551d
Connor O Meny <cm73189@srcs.loc>
2019-04-22 13:15:05 -0700

    Creating lifeboard, scoreboard, and char

--- GamePage.js
+++ GamePage.js
@@ -1,115 +1,123 @@
-window.addEventListener("keydown", shootLasers);
-window.addEventListener("keydown", moveCharacter);
+window.addEventListener("keydown", moveCharacter());
+window.addEventListener("keydown", pauseGame());
+window.addEventListener("keydown", shootLasers());
+
+var myPizzas = [];
 
 class AttackOfThePizzas {
-    constructor() {
-        this.player = new Player();
-        this.alienPizza = new AlienPizza();
-        this.pizza = new Pizza();
-        this.lifeboard = new Lifeboard();
-        this.scoreboard = new Scoreboard();
-    }
+  constructor() {
+    this.player = new Player();
+    this.alienPizza = new AlienPizza();
+    this.pizza = new Pizza();
+    this.lifeboard = new Lifeboard();
+    this.scoreboard = new Scoreboard();
+  }
 
-    updateLifeboard() {
+  pauseGame(event) {}
 
-    }
+  updateLifeboard() {}
 
-    displayLifeboard() {
+  displayLifeboard() {}
 
-    }
+  updateScoreboard() {}
 
-    updateScoreboard() {
+  displayScoreboard() {}
 
-    }
+  spawnAlien() {}
 
-    displayScoreboard() {
-
-    }
-
-    spawnAlien() {
-
-    }
-
-    spawnPizza() {
-
-    }
+  spawnPizza() {
+    let asteroid = new Pizza(
+      Math.random(0, window.height),
+      Math.random(0, window.height)
+    );
+  }
 }
 
 class Player {
-    constructor() {
-        this.speed = 0;
-        this.direction = 0;
-        this.turnAngle = 0;
-        this.laserSpeed = 0;
-    }
+  constructor() {
+    this.speedX = 0;
+    this.speedY = 0;
+    this.direction = 0;
+    this.turnAngle = 0;
+    this.laserSpeed = 0;
+    this.x = window / 2;
+    this.y = window / 2;
+    this.elem = document.getElementById("Player");
+  }
 
-    moveChararacter(event) {
-        if(event.key === "w") {
-           speed++;
-            console.log("w");
-       }
-        else if(event.key === "a") {
-           direction--;
-            console.log("a");
-       }
-        else if(event.key === "s") {
-           direction++;
-            console.log("s");
-       }
+  moveCharacter(event) {
+    var keyCode = event.keyCode;
+    if (keyCode === 87) {
+      //this.speed++;
+      console.log("w");
+    } else if (keyCode === 63) {
+      //this.direction--;
+      console.log("a");
+    } else if (keyCode === 68) {
+      //this.direction++;
+      console.log("s");
     }
+  }
 
-    shootLasers(event) {
-       if(event.code === "Space") {
-           //shoot
-           console.log("Pew");
-       }
+  shootLasers(event) {
+    if (keyCode === 32) {
+      //shoot
+      console.log("Pew");
     }
+  }
+
+  renderPlayer() {
+    this.elem.style.top = this.y + "px";
+    this.elem.style.left = this.x + "px";
+  }
 }
 
 class AlienPizza {
-    constructor() {
-        this.speed = 0;
-        this.direction = 0;
-        this.turnAngle = 0;
-        this.laserSpeed = 0;
-    }
+  constructor() {
+    this.speed = 0;
+    this.direction = 0;
+    this.turnAngle = 0;
+    this.laserSpeed = 0;
+    this.elem = document.getElementById(_id);
+  }
 
-    shootRandLasers() {
-        let randNum = Math.random()
-        if(randNum >= 0.75) {
-            console.log("Whirr");
-        }
+  shootRandLasers() {
+    let randNum = Math.random();
+    if (randNum >= 0.75) {
+      console.log("Whirr");
     }
+  }
+
+  renderShip() {
+    this.elem.style.top = this.y + "px";
+    this.elem.style.left = this.x + "px";
+  }
 }
 
 class Lifeboard {
-    constuctor() {
-        this.lives = 3;
-    }
+  constuctor() {
+    this.lives = 3;
+  }
 
-    updateLives() {
-
-    }
+  updateLives() {}
 }
 
 class Scoreboard {
-    constructor() {
-        this.score = 0;
-    }
+  constructor() {
+    this.score = 0;
+  }
 
-    updateScore() {
-
-    }
+  updateScore() {}
 }
 
 class Pizza {
-    constructor() {
-        this.levelOfSize = 1;
-        this.speed = 0;
-        this.direction = 0;
-    }
+  constructor(_xpos, _ypos) {
+    this.levelOfSize = 1;
+    this.speed = 0;
+    this.direction = 0;
+    this.x = _xpos;
+    this.y = _ypos;
+  }
 
-    breakOff() {
-
-    }
+  breakOff() {}
 }
