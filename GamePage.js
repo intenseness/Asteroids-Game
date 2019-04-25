f7873e27ee69308d070e04f9c059c44dad7a78cf
intenseness <connormeny@gmail.com>
2019-04-24 22:36:08 -0700

    Made thrust and turning (mostly done)

--- GamePage.js
+++ GamePage.js
@@ -14,17 +14,16 @@ class AttackOfThePizzas {
       if (event.keyCode == 81) {
         this.pauseGame();
       } else if (event.keyCode == 87) {
-        this.player.moveFoward();
+        this.player.thrust();
       } else if (event.keyCode == 65) {
-        this.player.turnLeft();
+        this.player.turnCCW();
       } else if (event.keyCode == 68) {
-        this.player.turnRight();
+        this.player.turnCW();
       } else if (event.keyCode == 32) {
         this.player.shootLasers();
       } else {
         this.player.angularVelocity = 0;
-        this.player.accelerationX = 0;
-        this.player.accelerationY = 0;
+        this.player.acceleration = 0;
       }
     });
   }
@@ -47,20 +46,34 @@ class Player {
   constructor() {
     this.speed_x = 0;
     this.speed_y = 0;
-    this.rotation_speed = 180; // degrees/second
-    this.acceleration = 200; // pixels/second/second
-    this.max_speed = 250; // pixels/second
-    this.laserSpeed = 0;
-    this.x = window.innerWidth / 2;
-    this.y = window.innerHeight / 2;
-    this.angle = -180;
     this.angularVelocity = 0;
+    //this.rotation = 0; // degrees/second
+    //this.acceleration = 200; // pixels/second/second
+    this.max_speed = 10; // pixels/second
+    this.x = 0; //window.innerWidth / 2;
+    this.y = 0; //window.innerHeight / 2;
+    this.angle = 0;
+    //this.length = Math.sqrt(this.x * this.x + this.y * this.y);
+    this.velocity = 0;
     this.elem = document.getElementById("Player");
+    //this.mass = 20;
+    //this.gravity = 10;
   }
 
   move() {
-      this.x = this.x + this.speed_x;
-      this.y = this.y + this.speed_y;
+      //this.x = this.x + this.speed_x;
+      //this.y = this.y + this.speed_y;
+
+      this.angle = this.angularVelocity + this.angle;
+      this.x += this.velocity * Math.sin(this.angle*Math.PI/180); //degrees to radians for both
+      this.y -= this.velocity * Math.cos(this.angle*Math.PI/180);
+
+      this.velocity *= 0.99; // drag
+      if(this.velocity < 0.01) {
+          this.velocity = 0;
+      }
+
+      //this.rotation = Math.sin(this.angle) * this.length;
       // Limit the movement here:
       /*if (this.x > window.width) {
         this.x = 0;
@@ -71,29 +84,40 @@ class Player {
         } else if (this.y < 0) {
         this.y = window.height;
         }*/
+      //console.log(this.angle, this.angularVelocity);
   }
 
   thrust() {
-      if(this.speed_x > this.max_speed) {
-          this.speed_x = this.max_speed;
-      } else if(this.speed_y > this.max_speed) {
-          this.speed_y = this.max_speed;
+    //let forces = this.gravity + this.max_speed;
+    //this.acceleration = forces / this.mass;
+      if(this.velocity < this.max_speed) {
+          this.velocity += 1;
       }
+    //this.velocity = 10;
+    //this.speed_x = this.velocity + this.speed_x;
+    //this.speed_y = this.velocity + this.speed_y;
+
+
+
     // What is our current angle...
     // Change speed_x and speed_y based on angle
     // Must limit to max speeds though
+
+      console.log(this.x, this.y, this.velocity, this.angle);
   }
 
   turnCCW() {
-    // Change angle
-    //this.angularVelocity = -this.rotation_speed;
-
-    //IMPORTANT: https://www.geeksforgeeks.org/2d-transformation-rotation-objects/
+      this.angle -= 4;
+       /*if(this.angularVelocity > -.3) {
+      this.angularVelocity -= 0.05;
+       }*/
   }
 
   turnCW() {
-    // Change angle
-    //this.angularVelocity = this.rotation_speed;
+      this.angle += 4;
+      /*if(this.angularVelocity < .3) {
+    this.angularVelocity += 0.05;
+      }*/
   }
 
   shootLasers() {
@@ -107,9 +131,10 @@ class Player {
   }
 
   render() {
-    this.elem.style.top = this.y + "px";
-    this.elem.style.left = this.x + "px";
-    // TODO: render the rotation too
+    //this.elem.style.top = this.y + "px";
+    //this.elem.style.left = this.x + "px";
+    this.elem.style.transform = "translate(" + this.x + "px, " + this.y + "px) rotateZ(" + this.angle + "deg)";
+    //this.elem.style.transform = 'rotateZ(' + this.angle + 'deg)';
   }
 
 }
