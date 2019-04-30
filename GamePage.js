d330edca8263f2af32195971ed0467cc36bdefdb
Connor O Meny <cm73189@srcs.loc>
2019-04-30 15:17:41 -0700

    Made collision better but need to use rects not circles

--- GamePage.js
+++ GamePage.js
@@ -1,15 +1,14 @@
 var myScore = 0;
-var myLives = 3;
-var myPizzas = [];
-var maxPizzas = 15;
-var numPizza = 1;
 var gameArea = document.getElementById("Game");
 
 class AttackOfThePizzas {
     constructor() {
         this.player = new Player();
-        this.pizza = new Pizza((Math.random() + 0.01) * 1700, (Math.random() + 0.01) * 900, (Math.random() + 0.1) * 5, (Math.random() + 0.1) * 5, 1);
+        this.pizzas = [];
+        this.maxPizzas = 5;
         this.scoreboard = new Scoreboard();
+        this.newPizzas();
+
         window.addEventListener("keydown", () => {
             if (event.keyCode == 87) {
                 this.player.thrust();
@@ -21,15 +20,48 @@ class AttackOfThePizzas {
         });
     }
 
+     newPizzas() {
+        for (let i = 0; i < this.maxPizzas; i++) {
+            let pizza = new Pizza(i, (Math.random() + 0.01) * 1700, (Math.random() + 0.01) * 850, (Math.random() + 0.1) * 5, (Math.random() + 0.1) * 5);
+            this.pizzas.push(pizza);
+        }
+    }
+
+    detectCollision() {
+        //use rectange one
+        for (let i = 0; i < this.pizzas.length; i++) {
+            let pizza = this.pizzas[i];
+            console.log("pizza", pizza.x, pizza.y);
+            console.log("player", this.player.x, this.player.y);
+            let dx = (pizza.x + pizza.width/2) - (this.player.x + this.player.width/2);
+            let dy = (pizza.y + pizza.height/2) - (this.player.y + this.player.height/2);
+
+            let distance = Math.sqrt(dx * dx + dy * dy);
+            console.log(dx, dy, distance);
+
+            if (distance < this.player.radius + this.pizzas[i].radius) {
+                console.log("Hit");
+                this.gameOver();
+            }
+        }
+    }
+
+    gameOver() {
+        let gameOver = document.createElement("h1");
+        gameOver.innerHTML = "Game Over";
+        gameArea.appendChild(gameOver);
+        clearInterval(id);
+    }
+
     update() {
         this.scoreboard.updateScore();
         this.player.move();
         this.player.render();
-        this.pizza.collision();
-        for (let i = 0; i < myPizzas.length; i++) {
-            myPizzas[i].move();
-            myPizzas[i].render();
+        for (let i = 0; i < this.pizzas.length; i++) {
+            this.pizzas[i].move();
+            this.pizzas[i].render();
         }
+        this.detectCollision();
     }
 }
 
@@ -44,7 +76,9 @@ class Player {
         this.angle = 0;
         this.velocity = 0;
         this.elem = document.getElementById("Player");
-        this.radius = 50;
+        this.radius = 40;
+        this.width = 40;
+        this.height = 40;
         this.elem.style.borderRadius = this.radius + "px";
     }
 
@@ -67,10 +101,10 @@ class Player {
             this.x = 0;
         } else if (this.x <= 0) {
             this.x = 1800;
-        } else if (this.y >= 900) {
+        } else if (this.y >= 700) {
             this.y = -20;
         } else if (this.y <= -20) {
-            this.y = 900;
+            this.y = 700;
         }
     }
 
@@ -95,13 +129,12 @@ class Player {
     render() {
         this.elem.style.top = this.y + "px";
         this.elem.style.left = this.x + "px";
-        this.elem.style.transform = "translate(" + this.x + "px, " + this.y + "px) rotateZ(" + this.angle + "deg)";
+        this.elem.style.transform = "rotateZ(" + this.angle + "deg)";
     }
 }
 
 class Pizza {
-    constructor(_xpos, _ypos, _speed_x, _speed_y, _sizelevel) {
-        this.levelOfSize = _sizelevel;
+    constructor(_id, _xpos, _ypos, _speed_x, _speed_y) {
         this.speed_x = _speed_x;
         this.speed_y = _speed_y;
         this.x = _xpos;
@@ -109,36 +142,21 @@ class Pizza {
         this.angle = Math.random() * 2 * Math.PI;
         this.div = document.createElement("div");
         this.elem = document.createElement("img");
-        this.div.id = "pizzaContainer" + numPizza;
-        this.elem.id = "pizza" + numPizza;
+        this.div.id = "pizzaContainer" + _id;
+        this.elem.id = "pizza" + _id;
         this.div.height = "90";
         this.div.width = "120";
         this.elem.height = "90";
         this.elem.width = "120";
-        this.radius = 120;
+        this.height = 90
+        this.width = 120
+        this.elem.style.border = "1px solid red";
+        this.radius = 40;
         this.elem.style.borderRadius = this.radius + "px";
         this.elem.src = "https://courthousepizzanashua.com/wp-content/uploads/2016/10/pizza-hut-cheese-pizza.jpg";
         this.div.style.position = "absolute";
         this.div.appendChild(this.elem);
         gameArea.appendChild(this.div);
-    }
-
-    collision() {
-        let dx = game.player.x - this.x;
-        let dy = game.player.y - this.y;
-        let distance = Math.sqrt(dx * dx + dy * dy);
-
-        if (distance < game.player.radius + this.radius) {
-            console.log("Hit");
-        }
-    }
-
-    newPizzas() {
-        for (let i = 0; i < maxPizzas; i++) {
-            let pizza = new Pizza((Math.random() + 0.01) * 1700, (Math.random() + 0.01) * 850, (Math.random() + 0.1) * 5, (Math.random() + 0.1) * 5, numPizza);
-            numPizza++;
-            myPizzas.push(pizza);
-        }
     }
 
     move() {
@@ -151,17 +169,17 @@ class Pizza {
             this.x = 0;
         } else if (this.x <= 0) {
             this.x = 1700;
-        } else if (this.y >= 900) {
+        } else if (this.y >= 700) {
             this.y = -60;
         } else if (this.y <= -60) {
-            this.y = 900;
+            this.y = 700;
         }
     }
 
     render() {
-        this.elem.style.top = this.y + "px";
-        this.elem.style.left = this.x + "px";
-        this.elem.style.transform = "translate(" + this.x + "px, " + this.y + "px)";
+        this.div.style.top = this.y + "px";
+        this.div.style.left = this.x + "px";
+        //this.elem.style.transform = "translate(" + this.x + "px, " + this.y + "px)";
     }
 }
 
@@ -177,8 +195,6 @@ class Scoreboard {
 
 let game = new AttackOfThePizzas();
 let id = setInterval(frame, 10);
-
-game.pizza.newPizzas();
 
 function frame() {
     myScore++;
